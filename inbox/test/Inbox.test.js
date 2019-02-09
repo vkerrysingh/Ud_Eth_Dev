bd27e4d8c00e4eed7d4621dd17aeb931d720dc9c
vkerrysingh <vkerrysingh@gmail.com>
2019-02-09 22:39:24 +0000

    Compiled and output compiled contract.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -8,7 +8,11 @@ const Web3 = require('web3');
 //must specify a provider for web3 to connect to ganache
 const web3 = new Web3(ganache.provider());
 
+//interface and bytecode come from the solc compile
+const {interface, bytecode} = require('../compile');
+
 let accounts;
+let inbox;
 
 //wait for accounts to be populated.
 beforeEach(async ()=>{
@@ -16,10 +20,14 @@ beforeEach(async ()=>{
   accounts = await web3.eth.getAccounts()
 
   //Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data:bytecode, arguments:['Hi there!'] })
+    .send({ from:accounts[0], gas:'1000000' })
 });
 
 describe('Inbox', () => {
   it('deploys a contract',() => {
-    console.log(accounts);
+    //console.log(accounts);
+    console.log(inbox);
   });
 })
