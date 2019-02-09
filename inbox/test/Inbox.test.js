cf00540956d01453fad49bb0af4fe601ff16dbfc
vkerrysingh <vkerrysingh@gmail.com>
2019-02-09 23:23:51 +0000

    Simple test for default message.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,18 +1,18 @@
 const assert = require('assert');
-
 //This will serve as our local ethereum test network
 const ganache = require('ganache-cli')
-
 //Constructor function. Hence note case.
 const Web3 = require('web3');
 //must specify a provider for web3 to connect to ganache
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 //interface and bytecode come from the solc compile
 const {interface, bytecode} = require('../compile');
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there!';
 
 //wait for accounts to be populated.
 beforeEach(async ()=>{
@@ -21,8 +21,10 @@ beforeEach(async ()=>{
 
   //Use one of those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data:bytecode, arguments:['Hi there!'] })
+    .deploy({ data:bytecode, arguments:[INITIAL_STRING] })
     .send({ from:accounts[0], gas:'1000000' })
+
+    inbox.setProvider(provider);
 });
 
 /*
@@ -39,4 +41,9 @@ describe('Inbox', () => {
     //ok method checks if a value exists. i.e is address a defined value
     assert.ok(inbox.options.address);
   });
+
+  it('has a default message', async () =>{
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING)
+  })
 })
