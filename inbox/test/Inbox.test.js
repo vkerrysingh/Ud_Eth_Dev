e35d9616537829810468175846bfa5fe05a85ce1
vkerrysingh <vkerrysingh@gmail.com>
2019-02-09 23:36:42 +0000

    Simple tests on Inbox contract.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -44,6 +44,12 @@ describe('Inbox', () => {
 
   it('has a default message', async () =>{
     const message = await inbox.methods.message().call();
-    assert.equal(message, INITIAL_STRING)
-  })
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it('can change the message', async () =>{
+    await inbox.methods.setMessage('bye').send({ from:accounts[0]});
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  });
 })
