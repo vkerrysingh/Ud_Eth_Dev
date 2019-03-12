d9b4b456503c7b6995c90b4316439ecbfe52decf
Varna Singh <vkerrysingh@gmail.com>
2019-03-12 19:33:45 +0000

    add a class and a state variable.

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -2,13 +2,18 @@ import React, {Component} from 'react';
 
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {term: '' };
+    }
 //every class must have a render function that returns JSX
     render() {
-        return <input onChange={this.onInputChange} />; //when there is a change in the input the pass in our handler
-    }
-
-    onInputChange(event) {
-        console.log(event.target.value);
+        return (
+            <div>
+                <input onChange={(event) => this.setState({term: event.target.value})} />
+            </div>
+        );
     }
 }
 
