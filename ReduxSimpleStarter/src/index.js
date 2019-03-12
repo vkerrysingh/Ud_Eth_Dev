9ce0845269db29176b6a148eb4c7954171ae19a7
Varna Singh <vkerrysingh@gmail.com>
2019-03-12 18:02:43 +0000

    rendering an instance of App to the div in index.html and swtiched to () function.

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 //Create a new component,it should produce some HTML
 //this is a class, we can have many instances of APP.
 //So will need to make an instance of APP
-const App = function (){
+const App = () => {
   return <div>Hi!</div>;
 }
 //Take the components generated HTML and put it on the page(in the DOM)
-ReactDOM.render(<App />); //this creates an instance of the class App
+ReactDOM.render(<App />, document.querySelector('.container')); //this creates an instance of the class App
