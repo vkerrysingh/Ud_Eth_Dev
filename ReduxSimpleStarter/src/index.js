95ef2ae5538cd1a6c1681566b2384211aea89889
Varna Singh <vkerrysingh@gmail.com>
2019-03-12 17:56:07 +0000

    rendering an example

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 //Create a new component,it should produce some HTML
+//this is a class, we can have many instances of APP.
+//So will need to make an instance of APP
 const App = function (){
   return <div>Hi!</div>;
 }
 //Take the components generated HTML and put it on the page(in the DOM)
-React.render(App);
+ReactDOM.render(<App />); //this creates an instance of the class App
