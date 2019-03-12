ea92b6eae8cdfe422180b0d2d80656e9ccd25d72
Varna Singh <vkerrysingh@gmail.com>
2019-03-12 19:09:25 +0000

    add event handler to the search

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    return <input />;
-};
+
+class SearchBar extends Component {
+//every class must have a render function that returns JSX
+    render() {
+        return <input onChange={this.onInputChange} />; //when there is a change in the input the pass in our handler
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+}
 
 //makes SearchBar availabe outside of the file
 export default SearchBar;
